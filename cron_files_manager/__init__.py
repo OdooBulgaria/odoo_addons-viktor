e538d836784c226c8775cf87317ed84fc494d6c7
straga <vostraga@gmail.com>
2015-01-31 19:07:15 +0200

    second commit

---
+++ cron_files_manager/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import fs_mover
+import YaDiskClient
+
+
---
+++ import_manager/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Import Manager Odoo',
+    'version' : '8.0.0.2',
+    'author' : 'Viktor Vorobjov',
+    'category': 'default',
+    'description' : """
+
+    
+    """,
+    'website' : 'http://www.prolv.net',
+    'depends' : ['base_setup','warning_popup', 'product','sale'],
+    'data': [
+        'ean_import_view.xml',
+    ],
+    'installable': True,
+}
+
